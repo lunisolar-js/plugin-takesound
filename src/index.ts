483f0a54efef072d646d8a0aa21ccf9f1c2d3b98
Natron Kan <waterbeside@163.com>
2023-02-13 16:07:48 +0800

    other: 删除多余代码

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,30 @@
+// @ts-check
+
 import { TAKE_SOUND_ELEMENT5 } from './constants'
 import type lunisolar from 'lunisolar'
 
-// import zh from './locale/zh'
+import zh from './locale/zh'
 
-interface SB extends lunisolar.SB {
-  _takeSoundValue: string
-  takeSound: string
-  takeSoundE5: lunisolar.Element5
-}
-
-interface LocaleDataEx extends LocaleData {
-  takeSound: string[]
-}
-
-interface LunisolarEx extends lunisolar.Lunisolar {
-  takeSound: string
-}
-
-const takeSoundPlugin: lunisolar.PluginFunc = async (options, lsClass, lsFactory) => {
-  // lsFactory.locale(zh, true)
-  const lsProto = lsClass.prototype as unknown as LunisolarEx
+export const takeSound: lunisolar.PluginFunc = async (options, lsClass, lsFactory) => {
+  lsFactory.locale(zh, true)
+  const lsProto = lsClass.prototype
   // **** 纳音 ****
-  const sbProto = lsFactory.SB.prototype as unknown as SB
+  const sbProto = lsFactory.SB.prototype
   // takeSound
   Object.defineProperty(sbProto, 'takeSound', {
     get(): string {
       const locale = lsFactory.getLocale(this._config.lang)
       if (this._takeSoundValue === undefined) {
-        this._takeSoundValue = ((this as lunisolar.SB).value >> 1) % 30
+        this._takeSoundValue = (this.value >> 1) % 30
       }
-      return (locale as LocaleDataEx).takeSound[this._takeSoundValue]
+      return locale.takeSound[this._takeSoundValue]
     }
   })
   // takeSoundE5
   Object.defineProperty(sbProto, 'takeSoundE5', {
     get(): lunisolar.Element5 {
       if (this._takeSoundValue === undefined) {
-        this._takeSoundValue = ((this as lunisolar.SB).value >> 1) % 30
+        this._takeSoundValue = (this.value >> 1) % 30
       }
       return new lsFactory.Element5(TAKE_SOUND_ELEMENT5[this._takeSoundValue], {
         lang: this._config.lang
@@ -49,7 +37,4 @@ const takeSoundPlugin: lunisolar.PluginFunc = async (options, lsClass, lsFactory
       return this.char8.day.takeSound
     }
   })
-
-  lsFactory
 }
-export default takeSoundPlugin
