53c3007ba877b35ee2cc6b9f464a99d57815ceca
Natron Kan <waterbeside@163.com>
2023-02-13 16:08:39 +0800

    test: 添加测试用例

---
+++ __tests__/index.test.ts
@@ -0,0 +1,31 @@
+import lunisolar from 'lunisolar'
+
+import { takeSound } from '../src/index'
+
+declare module 'lunisolar' {
+  interface Lunisolar {
+    takeSound: string
+  }
+  interface SB {
+    readonly _takeSoundValue: string
+    takeSound: string
+    takeSoundE5: lunisolar.Element5
+  }
+}
+
+lunisolar.extend(takeSound)
+
+describe('test takeSound', () => {
+  it('2022-07-08', () => {
+    const lsr = lunisolar('2022-07-08')
+    expect(lsr.takeSound).toBe('大海水')
+    expect(lsr.char8.year.takeSound).toBe('金箔金')
+    expect(lsr.char8.day.takeSound).toBe('大海水')
+
+    const lsr2 = lunisolar('2016-05-17')
+    expect(lsr2.takeSound).toBe('平地木')
+
+    const lsr3 = lunisolar('2022-1-1')
+    expect(lsr3.takeSound).toBe('大溪水')
+  })
+})
